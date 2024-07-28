c118c5ba33dd159f1a46780de224d8c58e71299b
unknown <vaibhavsharma2511@gmail.com>
2024-07-27 22:57:48 -0400

    adding result.html

--- app.py
+++ app.py
@@ -15,9 +15,17 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     features = [float(x) for x in request.form.values()]
-    features = np.array(features).reshape(1, -1)
-    prediction = model.predict(features)[0]
-    return render_template('index.html', prediction_text=f'Predicted Weight: {prediction:.2f} grams')
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+    prediction_text = f'Predicted Fish Weight: {prediction[0]:.2f}'
+
+    return render_template('result.html',
+                           prediction_text=prediction_text,
+                           Length1=features[0],
+                           Length2=features[1],
+                           Length3=features[2],
+                           Height=features[3],
+                           Width=features[4])
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
